86ff8b9fc0bd4aed237c00ffd20f04ae5f5b371b
kumarsandeep567 <kumar.sandee@northeastern.edu>
2025-11-27 02:22:08 -0500

    feat: add notification routes for managing notifications #57

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use('/auth', require('./auth'));
 router.use('/events', require('./event'));
 router.use('/upload', require('./upload'));
+router.use('/notifications', require('./notification'));
 router.use('/', require('./registration'));
 router.use('/', require('./comment'));
 
